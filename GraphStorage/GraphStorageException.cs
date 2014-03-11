2ed86be0850406418840fb01537c6539e31c98fe
Nate McMaster <nate.mcmaster@gmail.com>
2014-03-11 01:10:59 -0600

    Setup first storage operation

---
+++ GraphStorage/GraphStorageException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGraph.GraphStorage
+{
+    public class GraphStorageException : Exception
+    {
+        public GraphStorageException(string m) : base(m) { }
+        public GraphStorageException(string m, Exception e) : base(m, e) { }
+        public GraphStorageException(Exception e) : base("Storage failed",e) { }
+    }
+}
---
+++ GraphStorage/IGraphStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGraph.GraphStorage
+{
+    public interface IGraphStorage
+    {
+
+        bool TagExists(string tag);
+
+        void AddTag(string tag);
+    }
+}
